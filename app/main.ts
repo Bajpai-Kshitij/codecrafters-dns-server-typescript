5c2316d19e3a07925ae027af2ed466a4fe2e14e7
v-kshitij.bajpai <v-kshitij.bajpai@yatra.com>
2024-09-27 13:06:49 +0530

    test

--- app/main.ts
+++ app/main.ts
@@ -2,6 +2,7 @@ import * as dgram from "dgram";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
+console.log("finding right git")
 
 // Uncomment this block to pass the first stage
 //
