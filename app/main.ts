49d9328a9f200584de71b3792f3b25fc3e9141a5
v-kshitij.bajpai <v-kshitij.bajpai@yatra.com>
2024-09-27 16:09:13 +0530

    new changes

--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,22 @@
-import * as dgram from "dgram";
+import dgram, { IncomingPacket, SocketAsPromised } from "dgram-as-promised";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
-const udpSocket: dgram.Socket = dgram.createSocket("udp4");
-udpSocket.bind(2053, "127.0.0.1");
-// Uncomment this block to pass the first stage
-//
+// console.log("Logs from your program will appear here!");
 // const udpSocket: dgram.Socket = dgram.createSocket("udp4");
 // udpSocket.bind(2053, "127.0.0.1");
-//
+// // Uncomment this block to pass the first stage
+// //
+// // const udpSocket: dgram.Socket = dgram.createSocket("udp4");
+// // udpSocket.bind(2053, "127.0.0.1");
+// //
+// // udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
+// //     try {
+// //         console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
+// //         const response = Buffer.from("");
+// //         udpSocket.send(response, remoteAddr.port, remoteAddr.address);
+// //     } catch (e) {
+// //         console.log(`Error sending data: ${e}`);
+// //     }
+// // });
 // udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
 //     try {
 //         console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
@@ -17,12 +26,26 @@ udpSocket.bind(2053, "127.0.0.1");
 //         console.log(`Error sending data: ${e}`);
 //     }
 // });
-udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
-    try {
-        console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
-        const response = Buffer.from("");
-        udpSocket.send(response, remoteAddr.port, remoteAddr.address);
-    } catch (e) {
-        console.log(`Error sending data: ${e}`);
-    }
-});
+const udpSocket: SocketAsPromised = dgram.createSocket("udp4");
+
+udpSocket
+  .bind(2053, "127.0.0.1")
+  .then((bind) => console.log("Socket is connected to port 2053 : " + JSON.stringify(bind)))
+  .catch((err) => console.log("Error while connecting to socket " + JSON.stringify(err)));
+
+async function listener(listen: IncomingPacket) {
+  console.log(`Received message: ${listen.msg.toString()}`);
+  console.log(`Received ${JSON.stringify(listen.rinfo.size)} bytes`);
+
+  const byte = await udpSocket.send(listen.msg, 0, listen.rinfo.size, listen.rinfo.port, listen.rinfo.address);
+  console.log(`Message is sent (${JSON.stringify(byte)} bytes)`);
+}
+for await (const packet of udpSocket) {
+  listener(packet as IncomingPacket);
+  // Close socket if Ctrl-D is in the message
+  if (packet.msg.indexOf(4) !== -1) {
+    await udpSocket.close();
+  }
+}
+
+// udpSocket.recv().then((data) => listener(data as IncomingPacket));
